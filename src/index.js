211a176785e08cc131a3a458ecae7d4c9b30e953
Matthew Wright <matthew@smallhelm.com>
2018-10-30 15:10:43 -0500

    saliance graph

--- src/index.js
+++ src/index.js
@@ -1,21 +1,61 @@
+let _ = require('lodash')
 let cleanEvent = require('./cleanEvent')
+
+function noopMatcher (event, state) {
+  return {
+    match: true,
+    state: state
+  }
+}
 
 function SelectWhen () {
   let whens = []
 
-  function when (matcher, fn, initialState) {
-    if (typeof matcher.toMatcher === 'function') {
-      matcher = matcher.toMatcher()
+  let salianceGraph = {}
+
+  function addSaliance (e, id) {
+    let domain = e.domain || '*'
+    let name = e.name || '*'
+    if (!salianceGraph[domain]) {
+      salianceGraph[domain] = {}
     }
-    // TODO saliance graph
-    let obj = {
-      matcher,
-      fn,
-      state: Object.freeze(initialState)
+    if (!salianceGraph[domain][name]) {
+      salianceGraph[domain][name] = []
     }
-    whens.push(obj)
+    salianceGraph[domain][name].push(id)
+  }
+
+  let nextWhenI = 0
+
+  function when (conf, fn) {
+    if (typeof conf.toMatcher === 'function') { // i.e. StateMachine method
+      conf = conf.toMatcher()
+    }
+    let matcher = noopMatcher
+    if (typeof conf.matcher === 'function') {
+      matcher = conf.matcher
+    } else if (typeof conf === 'function') {
+      matcher = conf
+    }
+
+    let id = 'w' + (nextWhenI++)
+
+    if (Array.isArray(conf.saliance)) {
+      conf.saliance.forEach(function (e) {
+        addSaliance(e, id)
+      })
+    } else {
+      addSaliance({}, id)// default to *:*
+    }
+
+    let state = Object.freeze(conf.initialState || {})
+
+    let obj = { id, matcher, fn, state }
+
+    whens[id] = obj
 
     return {
+      id,
       setState: function (state) {
         obj.state = Object.freeze(state)
       },
@@ -28,7 +68,13 @@ function SelectWhen () {
   function emit (event) {
     event = cleanEvent(event)
 
-    whens.forEach(function (when) {
+    let salient = _.uniq(_.get(salianceGraph, [event.domain, event.name], [])
+      .concat(_.get(salianceGraph, [event.domain, '*'], []))
+      .concat(_.get(salianceGraph, ['*', event.name], []))
+      .concat(_.get(salianceGraph, ['*', '*'], [])))
+
+    salient.forEach(function (id) {
+      let when = whens[id]
       let resp = when.matcher(event, when.state)
       when.state = Object.freeze(resp.state)
       if (resp.match === true) {
--- test/index.js
+++ test/index.js
@@ -7,18 +7,21 @@ test('basics', function (t) {
 
   let matches = []
 
-  let w0 = hub.when(function (event, state) {
-    t.true(Object.isFrozen(event))
-    t.true(Object.isFrozen(state))
-    return {
-      match: true,
-      state: { n: state.n + 1 }
+  let w0 = hub.when({
+    initialState: { n: 0 },
+    matcher: function (event, state) {
+      t.true(Object.isFrozen(event))
+      t.true(Object.isFrozen(state))
+      return {
+        match: true,
+        state: { n: state.n + 1 }
+      }
     }
   }, function (event, state) {
     t.true(Object.isFrozen(event))
     t.true(Object.isFrozen(state))
     matches.push([ _.omit(event, 'time'), state ])
-  }, { n: 0 })
+  })
 
   hub.emit('aa')
   hub.emit('bb:cc')
@@ -35,4 +38,38 @@ test('basics', function (t) {
     [{ domain: null, name: 'dd', data: { attr: 1 } }, { n: 3 }],
     [{ domain: null, name: 'ee', data: null }, { n: 101 }]
   ])
+})
+
+test('saliance graph', function (t) {
+  let hub = SelectWhen()
+
+  let askedToMatch = []
+  let matches = []
+
+  hub.when({
+    saliance: [
+      { domain: 'foo', name: 'foo' },
+      { domain: 'bar', name: '*' }
+    ],
+    matcher: function (event, state) {
+      askedToMatch.push(event.domain + ':' + event.name)
+      return { match: true, state }
+    }
+  }, function (event, state) {
+    matches.push(event.domain + ':' + event.name)
+  })
+
+  hub.emit('foo:foo')
+  hub.emit('foo:bar')
+  hub.emit('bar:bar')
+  hub.emit('wat:bar')
+  hub.emit('bar:wat')
+
+  // should only be askedToMatch the ones that are salient
+  t.deepEqual(askedToMatch, [
+    'foo:foo',
+    'bar:bar',
+    'bar:wat'
+  ])
+  t.deepEqual(askedToMatch, matches)
 })
